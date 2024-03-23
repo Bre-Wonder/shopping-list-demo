892904b60f65ed921613988ee85064d3fbab16d8
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-23 12:31:03 -0700

    configured Firebase to run inside mobile app

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
+import { NavigationContaier } from '@react-navigation/native';
+import { createNativeNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const App = () => {
+
+  // Web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBiWTOdCv287aYrqzqKInHAPsWSkZsbfF0",
+    authDomain: "shopping-list-demo-a678c.firebaseapp.com",
+    projectId: "shopping-list-demo-a678c",
+    storageBucket: "shopping-list-demo-a678c.appspot.com",
+    messagingSenderId: "497500707938",
+    appId: "1:497500707938:web:f91612197ce0499ca7053b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  //Initialize Cloud Firestore and get a referense to the service
+  const db = getFirestore(app);
+
+  return(
+    <NavigationContaier>
+      <Stack.Navigator>
+      </Stack.Navigator>
+    </NavigationContaier>
+  )
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
