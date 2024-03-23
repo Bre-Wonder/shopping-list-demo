c4f3ad016f4f329391cbbf83aa6e7cefcb37efea
Bre-Wonder <117392929+Bre-Wonder@users.noreply.github.com>
2024-03-22 18:31:01 -0700

    imported Firebase into app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
